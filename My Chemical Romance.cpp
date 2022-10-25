f2e6025aa4841777dd33ef2885d2cf4f917cb78b
OyelDaniel <102643280+OyelDaniel@users.noreply.github.com>
2022-10-25 23:49:23 +0800

    Add files via upload

---
+++ My Chemical Romance.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+printf("My Chemical Romance");
+printf("\n\n\tThere is a band that most kids today (from where i am from) do not know. This band has saved the lives of thousands of people through their music.");
+printf("\n\n\tMy Chemical Romance, this band means more to me than words can ever say. They have changed the world, and they have changed me. Whenever I think of"
+"an amazing band, I think about My Chemical Romance. I have never before been able to make sense of these words until I know My Chemical Romance. Beautiful,"
+"stunning, perfectly \nflawed, intelligent, funny, helpful, lifesaving, and the list goes on. This band has saved my life and many others.");
+printf("\n\n\tMy Chemical Romance is often shortened to MCR or My Chem by fans. The reason I chose to write about them is because, there were times in my life "
+"when I felt \nlike an outcast, like if I was pushed down and could not get up, times when people made fun of me, times when my parents were mad at me, and all of"
+" those times the \nthing that told me not to feel so bad and to never give up was My Chemical Romance music.");
+printf(" Most people think MCR sings about death and self-harm, but if you really \nlisten to their lyrics you can see that they sing about having faith and hope"
+" and not to give up even when you feel like you will never get back up. Here is an \nexample of some lyrics, 'I am not afraid to keep on living. I am not afraid."
+"to walk this world alone. Honey, if you stay I will be forgiven. Nothing you can say can \nstop me going home'Their second album, Three Cheers For Sweet Revenge,"
+" is all about standing up for yourself and not being afraid to be yourself");
+printf("\n\n\tAnother reason I chose to write about MCR is, the lead singer Gerard Way fight through depression. He showed all the fans that even when your down"
+" at your \nlowest and nobody thinks you can get back up and everyone thinks you are close to the end, you can get back up and amaze everyone. Gerard has since"
+" then gotten over \nhis depression.");
+printf("\n\n\tThat concludes the band of My Chemical Romance. You are not expected to understand the reason for this band fame if you are not a fan yourself. I"
+" look up to \nthem, I respect them, I long to meet them, and I recognize them as the newest kings of alternative rock.");
+printf("They were the ones who told me 'If you look in the mirror and do not like what you see, you can find out first hand what it is like to be me.' Even now"
+" the words I am typing are only in black and white, without overwhelming \nemotion or colorful feelings. There is no possible way for these feelings to be put"
+" on paper, they can only thrive in my heart.");
+printf("This one is for the boys of My ChemicalRomance, who rose to the occasion, instilled a sense of belonging in my heart and make me have hope.");
+printf("\n\n\tI think that music can change the world, and has certainly had an impact on the lives of thousand. It is one of the things that has made an impact"
+" on me.");
+    return 0;
+}
